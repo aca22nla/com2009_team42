f33ddb92a1e8a2e5c4216443edd8cd4a547acaea
Yusaf M <ymuthana1@sheffield.ac.uk>
2024-02-28 13:43:50 +0000

    added odometry data (unfinshed)

--- src/move_circle.py
+++ src/move_circle.py
@@ -3,6 +3,10 @@
 import rospy 
 from geometry_msgs.msg import Twist
 
+from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion
+
+
 class Circle():
 
     def __init__(self):
@@ -12,12 +16,43 @@ class Circle():
         rospy.init_node(self.node_name, anonymous=True)
         self.rate = rospy.Rate(10) 
 
+        self.sub = rospy.Subscriber("odom", Odometry, self.callback)
+
         self.ctrl_c = False 
-        rospy.on_shutdown(self.shutdownhook) 
+        rospy.on_shutdown(self.shutdownhook)
+
+        self.counter = 11
 
         self.direction = 1  # Direction of rotation: 1 for counterclockwise, -1 for clockwise
         self.circle_count = 0  # Count of completed circles
         rospy.loginfo(f"The '{self.node_name}' node is active...") 
+
+    def callback(self, topic_data: Odometry):
+        pose = topic_data.pose.pose
+        position = pose.position
+        orientation = pose.orientation 
+        
+        initial_pos_x = None
+        initial_pos_y = None
+        initial_pos_z = None
+        pos_x = position.x #forward backwards
+        pos_y = position.y #right left
+        pos_z = position.z #up down
+        if initial_pos_x == None:
+            initial_pos_x = pos_x
+        if initial_pos_y == None:
+            initial_pos_y = pos_y
+        if initial_pos_z == None:
+            initial_pos_z = pos_z
+
+        (roll, pitch, yaw) = euler_from_quaternion([orientation.x, orientation.y, orientation.z, orientation.w], 'sxyz')
+
+        
+        if self.counter > 10:
+            self.counter = 0
+            print(f"x={pos_x-initial_pos_y:.2f} [m], y={pos_y-initial_pos_y:.2f} [m], yaw={yaw:.1f} [degrees].")
+        else:
+            self.counter += 1
 
     def shutdownhook(self):
         stop_cmd = Twist()

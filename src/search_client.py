188871b547fe31a3ba52db18c4b4e46fe645d4ca
TUoS Student <student@Tisya.>
2024-03-15 00:42:56 +0000

    robot searches area but is unable to avoid obstacles effectively

--- src/search_client.py
+++ src/search_client.py
@@ -55,7 +55,7 @@ class ExplorerClient():
     def main(self):
         ## TODO: assign values to all goal parameters
         ## and send the goal to the action server...
-        self.send_goal(fwd_vel= 0.1, dist = 0.3) 
+        self.send_goal(fwd_vel= 0.05, dist = 0.3) 
         #debug
         print("Goal parameters set successfully:")
         print("Forward velocity:", self.goal.fwd_velocity)

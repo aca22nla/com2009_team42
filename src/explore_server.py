e9d84814988946d1a474e3ed5c040b9d90045af4
aca22nla <nlbalfian1@sheffield.ac.uk>
2024-05-03 09:36:03 +0100

    improved movement for task 4

--- src/explore_server.py
+++ src/explore_server.py
@@ -4,10 +4,10 @@
 import rospy
 import actionlib
 import waffle
+import math
 
 
 from tuos_msgs.msg import SearchAction, SearchFeedback, SearchResult, SearchGoal
-from math import sqrt, pow, radians
 
 
 
@@ -19,37 +19,46 @@ class RobotExplorer():
         self.server_name = "search_action_server"
         rospy.init_node(self.server_name)
 
-        # functions from the tb3.py module:
-        self.vel_controller = waffle.Motion(debug = True)
-        self.tb3_odom = waffle.Pose(debug = True)
-        self.tb3_lidar = waffle.Lidar(debug = True)
-        rospy.loginfo("tb3_lidar initialized successfully")
+        # functions from the waffle.py module:
+        self.motion = waffle.Motion(debug = True)
+        self.pose = waffle.Pose(debug = True)
+        self.lidar = waffle.Lidar(debug = True)
+        rospy.loginfo("Lidar initialized successfully")
 
         self.actionserver = actionlib.SimpleActionServer(self.server_name, SearchAction,
                                                           self.action_server_launcher, auto_start=False)
         self.actionserver.start()
 
-        self.min_safe_distance = 0.3
-        self.max_angular_velocity = radians(35)  # Maximum angular velocity in radians per second
-        self.turn_threshold = radians(10)  # Angle threshold for initiating a turn
+        self.min_safe_distance = 0.5
+        self.max_angular_velocity = math.radians(35)  # Maximum angular velocity in radians per second
+        self.turn_threshold = math.radians(10)  # Angle threshold for initiating a turn
 
         rospy.loginfo("The 'Search Action Server' is active...")
 
-    def calculate_angular_velocity(self):
-        # Calculate angular velocity based on laser scan data
-        if self.closest_object > self.min_safe_distance:
-            # No obstacles nearby, move forward
-            return 0.0
-        elif abs(self.closest_object_location) < self.turn_threshold:
-            # Obstacle detected near the front, initiate a turn
-            return self.max_angular_velocity
-        else:
-            # Obstacle detected, calculate angular velocity to follow the gap
-            angular_velocity = (self.closest_object_location - 0) / 20.0  # proportional control
-            return angular_velocity
+    def calculate_angular_velocity(self, closest_object_location):
+        orientation_difference = closest_object_location - self.pose.yaw
+        # Normalize the orientation difference to the range [-pi, pi]
+        orientation_difference = (orientation_difference + math.pi) % (2 * math.pi) - math.pi
+        
+        # Calculate angular velocity based on the orientation difference
+        return orientation_difference * 0.5  # Adjust the coefficient as needed
+
+
+    # def calculate_angular_velocity(self):
+    #     # Calculate angular velocity based on laser scan data
+    #     if self.closest_object > self.min_safe_distance:
+    #         # No obstacles nearby, move forward
+    #         return 0.0
+    #     elif abs(self.closest_object_location) < self.turn_threshold:
+    #         # Obstacle detected near the front, initiate a turn
+    #         return self.max_angular_velocity
+    #     else:
+    #         # Obstacle detected, calculate angular velocity to follow the gap
+    #         angular_velocity = (self.closest_object_location - 0) / 20.0  # proportional control
+    #         return angular_velocity
 
             
-    def action_server_launcher(self, goal: SearchGoal):
+    def action_server_launcher(self, goal:SearchGoal):
         rate = rospy.Rate(10)
 
         # Print the received goal for debugging
@@ -71,61 +80,54 @@ class RobotExplorer():
         if not success:
             # abort the action server if an invalid goal has been requested...
             rospy.logerr("Aborting action due to invalid goal...")
-            self.result.closest_object_distance = 0
-            self.result.total_distance_travelled = 0
-            self.result.closest_object_angle = 0
-            self.actionserver.set_aborted(self.result)
+            self.actionserver.set_aborted(SearchResult())
             return
         #if requested goal is valid
         print(f"Valid goal. Continue action...")
 
 
         while not rospy.is_shutdown():
-            # Get the robot's current odometry from the Tb3Odometry() class:
-            self.posx0 = self.tb3_odom.posx
-            self.posy0 = self.tb3_odom.posy
-            # Get information about objects up ahead from the Tb3LaserScan() class:
-            self.closest_object = self.tb3_lidar.subsets.front #distance
+            # Get the robot's current odometry
+            posx0 = self.pose.posx
+            posy0 = self.pose.posy
+
+            # Check LiDAR data
+            closest_object_distance = min(self.lidar.subsets.frontArray)
+
             # self.closest_object_location = self.tb3_lidar.closest_object_position #angle
 
-            min_distance_index = self.tb3_lidar.subsets.frontArray.index(min(self.tb3_lidar.subsets.frontArray))
-            angle_increment = 0.5
-            self.closest_object_location = (min_distance_index - len(self.tb3_lidar.subsets.frontArray) / 2) * angle_increment
-            
+            #check if there is object detected
+            if closest_object_distance <= self.min_safe_distance:
+                rospy.loginfo("Obstacle detected. Avoiding obstacle")
+
+                # Calculate angular velocity to avoid the obstacle
+                min_distance_index = self.lidar.subsets.frontArray.index(closest_object_distance)
+                angle_increment = 0.5
+                closest_object_location = (min_distance_index - len(self.lidar.subsets.frontArray) / 2) * angle_increment
+                angular_velocity = self.calculate_angular_velocity(closest_object_location)
+                self.motion.set_velocity(linear=0.0, angular=angular_velocity)
+                self.motion.publish_velocity()
+
+                rospy.sleep(0.5)  # Allow the robot to turn for 1 second
+
+            else:
+                # No obstacle detected, continue exploration
+                self.motion.set_velocity(linear=0.2, angular=0.0)
+                self.motion.publish_velocity()
+
             # check if there has been a request to cancel the action mid-way through:
             if self.actionserver.is_preempt_requested():
                 rospy.loginfo("Action preempted. Cancelling goal...")
                 self.actionserver.set_preempted(self.result)
                 # Stop the robot
-                self.vel_controller.stop()
+                self.motion.stop()
                 success = False
                 # exit the loop:
                 break
 
-            if self.closest_object <= self.min_safe_distance:
-                rospy.loginfo("Obstacle detected. Avoiding obstacle.")
-
-                # Stop the robot
-                self.vel_controller.stop()
-                rospy.sleep(1)  # Pause for 1 second
-
-                # Calculate angular velocity to avoid the obstacle
-                angular_velocity = self.calculate_angular_velocity()  # Turn in the opposite direction
-                self.vel_controller.set_velocity(linear=0.0, angular=angular_velocity)
-                self.vel_controller.publish_velocity()
-
-                rospy.sleep(1)  # Turn for 1 seconds
-
-            else:
-                # no obstacle detected, continue exploration
-                angular_velocity = self.calculate_angular_velocity()
-                # Set angular velocity to navigate the robot
-                self.vel_controller.set_velocity(linear=0.1, angular=angular_velocity)
-                # Publish velocity command
-                self.vel_controller.publish_velocity()
 
             # determine how far the robot has travelled so far:
-            self.distance = sqrt(pow(self.posx0 - self.tb3_odom.posx, 2) + pow(self.posy0 - self.tb3_odom.posy, 2))
+            self.distance = math.sqrt(pow(posx0 - self.pose.posx, 2) + pow(posy0 - self.pose.posy, 2))
 
             # update feedback message values and publish feedback:
             self.feedback.current_distance_travelled = self.distance
@@ -134,15 +136,15 @@ class RobotExplorer():
 
             # update all result parameters:
             self.result.total_distance_travelled = self.distance
-            self.result.closest_object_angle = self.closest_object_location
-            self.result.closest_object_distance = self.closest_object
+            # self.result.closest_object_angle = self.closest_object_location
+            # self.result.closest_object_distance = self.closest_object
 
             rate.sleep()
             
         if success:
             rospy.loginfo("approach completed successfully.")
             self.actionserver.set_succeeded(self.result)
-            self.vel_controller.stop()
+            self.motion.stop()
 
     def main(self):
         rospy.spin()
